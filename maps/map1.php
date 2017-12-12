adcdd6ac0e8f57ccd704eed61c21bcc727ff0896
AdrianMarceau <adrian.marceau@gmail.com>
2017-12-11 21:40:25 -0500

    Updated map1 with newly shifted position.

--- maps/map1.php
+++ maps/map1.php
@@ -1,19 +1,19 @@
 <?php
 
-// Define the paths that should appear on the map
-$map_canvas_paths[1][2] = 'l-stub';
-$map_canvas_paths[2][2] = 'h-line';
-$map_canvas_paths[3][2] = 'h-line';
-$map_canvas_paths[4][2] = 'h-line';
-$map_canvas_paths[5][2] = 'h-line';
-$map_canvas_paths[6][2] = 'cross';
-$map_canvas_paths[7][2] = 'r-stub';
-$map_canvas_paths[6][1] = 't-stub';
-$map_canvas_paths[6][3] = 'b-stub';
+// Define the paths that should appear on the map (x9)
+$map_canvas_paths[7][2] = 't-stub';
+$map_canvas_paths[2][3] = 'l-stub';
+$map_canvas_paths[3][3] = 'h-line';
+$map_canvas_paths[4][3] = 'h-line';
+$map_canvas_paths[5][3] = 'h-line';
+$map_canvas_paths[6][3] = 'h-line';
+$map_canvas_paths[7][3] = 'cross';
+$map_canvas_paths[8][3] = 'r-stub';
+$map_canvas_paths[7][4] = 'b-stub';
 
-// Define the battles that should appear on the map
-$map_canvas_battles[2][2] = 'mecha/intro-field';
-$map_canvas_battles[4][2] = 'master/light-laboratory';
-$map_canvas_battles[6][2] = 'boss/prototype-subspace';
+// Define the battles that should appear on the map (x3)
+$map_canvas_battles[3][3] = 'mecha/intro-field';
+$map_canvas_battles[5][3] = 'master/light-laboratory';
+$map_canvas_battles[7][3] = 'boss/prototype-subspace';
 
 ?>
